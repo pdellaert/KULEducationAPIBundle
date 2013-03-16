e86e5b7d66d8876ba70236728645e13acb25276a
Philippe Dellaert <philippe@dellaert.org>
2013-03-16 14:24:15 +0100

    testing

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -2,11 +2,13 @@
 namespace Dellaert\KULEducationXMLBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends Controller
 {
 	public function indexAction()
 	{
-		return $this->render('DellaertKULEducationXMLBundle:Main:index.html.twig');
+		$baseurl = $this->container->getParameter('dellaert_kul_education_xml.baseurl');
+		return $this->render('DellaertKULEducationXMLBundle:Main:index.html.twig',array('baseurl'=>$baseurl));
 	}
 }
