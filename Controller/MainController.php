a761c51699532f09513808fc9849293941bc82bc
Philippe Dellaert <philippe@dellaert.org>
2013-03-16 22:32:28 +0100

    adding levels, then studies, reorganisation

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -42,7 +42,7 @@ class MainController extends Controller {
     	return $response;
 	}
 
-	public function listStudiesByIdTitleAction($fid) {
+	public function listLevelsByIdTitleAction($fid) {
 		// Locale
 		$language = substr($this->getRequest()->getLocale(),0,1);
 
@@ -60,9 +60,49 @@ class MainController extends Controller {
 			$faculty = $xml->xpath("data/instelling/departement[@objid='$fid']");
 
 			if( !empty($faculty) ) {
-				foreach( $faculty[0]->xpath("classificaties/classificatie/graad/diplomas/diploma") as $fChild ){
-					if( ((string) $fChild->originele_titel['taal']) == $language ) {
-						$data[] = array('id'=>(string) $fChild['objid'],'title'=>(string) $fChild->originele_titel);
+				foreach( $faculty[0]->xpath("classificaties/classificatie/graad") as $fChild ){
+					$title = $fChild->xpath("omschrijvingen/omschrijving[@taal='$language']");
+
+					if( empty($title) ) {
+						$title = $fChild->xpath("omschrijvingen/omschrijving[@taal='".$this->container->getParameter('dellaert_kul_education_xml.fallback_locale')."']");
+					}
+
+					$data[] = array('id'=>(string) $fChild['objid'],'title'=>(string) $title[0]);
+				}
+			}
+		}
+
+		// Returning studies
+		$response = new Response(json_encode($data));
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
+	}
+
+	public function listStudiesByIdTitleAction($fid,$lid) {
+		// Locale
+		$language = substr($this->getRequest()->getLocale(),0,1);
+
+		// Return value
+		$data = array();
+
+		// URL Setup
+		$url = $this->container->getParameter('dellaert_kul_education_xml.baseurl');
+		$year = $this->container->getParameter('dellaert_kul_education_xml.baseyear');
+		$method = $this->container->getParameter('dellaert_kul_education_xml.method');
+		$callUrl = $url.$year.'/opleidingen/n/'.$method.'/index.xml';
+		
+		// Getting XML
+		if( $xml = simplexml_load_file($callUrl, null, LIBXML_NOCDATA) ) {
+			$faculty = $xml->xpath("data/instelling/departement[@objid='$fid']");
+
+			if( !empty($faculty) ) {
+				$level = $faculty->xpath("classificaties/classificatie/graad[@id=='$lid']");
+
+				if( !empty($level) ) {
+					foreach( $level[0]->xpath("diplomas/diploma") as $fChild ){
+						if( ((string) $fChild->originele_titel['taal']) == $language ) {
+							$data[] = array('id'=>(string) $fChild['objid'],'title'=>(string) $fChild->originele_titel);
+						}
 					}
 				}
 			}
