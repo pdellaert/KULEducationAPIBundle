8575941e0ceaeb7898798234525f837d9fb772ea
Philippe Dellaert <philippe@dellaert.org>
2013-03-16 14:04:50 +0100

    translation test

---
+++ Controller/MainController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Dellaert\KULEducationXMLBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('DellaertKULEducationXMLBundle:Main:index.html.twig');
+    }
+}
--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -4,14 +4,14 @@ namespace Dellaert\KULEducationXMLBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class MainControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $client = static::createClient();
+        //$client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        //$crawler = $client->request('GET', '/hello/Fabien');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        //$this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
     }
 }
