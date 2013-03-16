7da1772662dc435091e6efa57701699e65259716
Philippe Dellaert <philippe@dellaert.org>
2013-03-16 20:15:19 +0100

    Testing AJAX and json

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -5,14 +5,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends Controller {
+
 	public function indexAction() {
 		$lang = substr($this->getRequest()->getLocale(),0,1);
 		return $this->render('DellaertKULEducationXMLBundle:Main:index.html.twig',array('faculties'=>$this->getFaculties($lang)));
 	}
 
-	public function getFaculties($language="n") {
+	public function listFacultiesIdTitleAction() {
+		// Locale
+		$language = substr($this->getRequest()->getLocale(),0,1);
+
 		// Return value
-		$fac = array();
+		$data = array();
 
 		// URL Setup
 		$url = $this->container->getParameter('dellaert_kul_education_xml.baseurl');
@@ -22,18 +26,50 @@ class MainController extends Controller {
 		
 		// Getting XML
 		if( $xml = simplexml_load_file($callUrl, null, LIBXML_NOCDATA) ) {
-			foreach( $xml->xpath('data/instelling/departement') as $fChild ) {
+			foreach( $xml->xpath("data/instelling/departement") as $fChild ) {
 				$title = $fChild->xpath("titels/titel[@taal='$language']");
 
 				if( empty($title) ) {
 					$title = $fChild->xpath("titels/titel[@taal='".$this->container->getParameter('dellaert_kul_education_xml.fallback_locale')."']");
 				}
 
-				$fac[] = array('id'=>(string) $fChild['objid'],'title'=>(string) $title[0]);
+				$data[] = array('id'=>(string) $fChild['objid'],'title'=>(string) $title[0]);
 			}
 		}
 
 		// Returning faculties
-		return $fac;
+		$response = new Response(json_encode($data));
+    	$response->headers->set('Content-Type', 'application/json');
+	}
+
+	public function listStudiesIdTitleAction($fid) {
+		// Locale
+		$language = substr($this->getRequest()->getLocale(),0,1);
+
+		// Return value
+		$$data = array();
+
+		// URL Setup
+		$url = $this->container->getParameter('dellaert_kul_education_xml.baseurl');
+		$year = $this->container->getParameter('dellaert_kul_education_xml.baseyear');
+		$method = $this->container->getParameter('dellaert_kul_education_xml.method');
+		$callUrl = $url.$year.'/opleidingen/n/'.$method.'/index.xml';
+		
+		// Getting XML
+		if( $xml = simplexml_load_file($callUrl, null, LIBXML_NOCDATA) ) {
+			$faculty = $xml->xpath("data/instelling/departement[@objid='$fid']");
+
+			if( !empty($faculty) ) {
+				foreach( $faculty[0]->xpath("classificaties/classificatie/graad/diplomas/diploma") as $fChild ){
+					if( ((string) $fChild->originele_titel['taal']) == $language ) {
+						$stud[] = array('id'=>(string) $fChild['objid'],'title'=>(string) $fChild->originele_titel);
+					}
+				}
+			}
+		}
+
+		// Returning studies
+		$response = new Response(json_encode($data));
+    	$response->headers->set('Content-Type', 'application/json');
 	}
 }
