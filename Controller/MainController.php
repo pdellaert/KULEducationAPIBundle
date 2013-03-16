91fcb0872f5f62f00a65a9b1b1f67d232d527d6c
Philippe Dellaert <philippe@dellaert.org>
2013-03-16 14:51:01 +0100

    Listing faculties

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -8,7 +8,28 @@ class MainController extends Controller
 {
 	public function indexAction()
 	{
-		$baseurl = $this->container->getParameter('dellaert_kul_education_xml.baseurl');
-		return $this->render('DellaertKULEducationXMLBundle:Main:index.html.twig',array('baseurl'=>$baseurl));
+		return $this->render('DellaertKULEducationXMLBundle:Main:index.html.twig',array('faculties'=>$this->getFaculties()));
+	}
+
+	public function getFaculties() {
+		// Return value
+		$fac = array();
+
+		// URL Setup
+		$url = $this->container->getParameter('dellaert_kul_education_xml.baseurl');
+		$year = $this->container->getParameter('dellaert_kul_education_xml.baseyear');
+		$language = substr($this->getRequest()->getSession()->get('_locale'),0,1);
+		$method = $this->container->getParameter('dellaert_kul_education_xml.method');
+		$callUrl = $url.$year.'/opleidingen/'.$language.'/'.$method.'/index.xml'; 
+		
+		// Getting XML
+		if( $xml = simplexml_load_file($callUrl, null, LIBXML_NOCDATA) ) {
+			foreach( $xml->xpath('data/instelling/departement') as $fChild ) {
+				$fac[] = array('id'=>(string) $fChild['objid'],'title'=>(string) $fChild->titels->titel);
+			}
+		}
+
+		// Returning faculties
+		return $fac;
 	}
 }
