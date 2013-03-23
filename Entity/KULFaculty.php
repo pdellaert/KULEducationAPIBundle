ee8c99e6d61033172a67be6c393c9c829cb80e25
Philippe Dellaert <philippe@dellaert.org>
2013-03-23 14:25:39 +0100

    First entity

---
+++ Entity/KULFaculty.php
@@ -0,0 +1,15 @@
+<?php
+namespace Dellaert\KULEducationAPIBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints as DoctrineAssert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="kulapi_faculty")
+ */
+class KULFaculty
+{
+}
