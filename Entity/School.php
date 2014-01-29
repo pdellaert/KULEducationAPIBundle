52d1a451ecdd84b6f51c5d2d4d06cd051903bb62
Philippe Dellaert <philippe@dellaert.org>
2014-01-29 23:34:17 +0100

    Getters and Setters

--- Entity/School.php
+++ Entity/School.php
@@ -45,4 +45,83 @@ class School
      */
     protected $baseURL;
 
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set shortname
+     *
+     * @param string $shortname
+     * @return School
+     */
+    public function setShortname($shortname)
+    {
+        $this->shortname = $shortname;
+
+        return $this;
+    }
+
+    /**
+     * Get shortname
+     *
+     * @return string 
+     */
+    public function getShortname()
+    {
+        return $this->shortname;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return School
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set baseURL
+     *
+     * @param string $baseURL
+     * @return School
+     */
+    public function setBaseURL($baseURL)
+    {
+        $this->baseURL = $baseURL;
+
+        return $this;
+    }
+
+    /**
+     * Get baseURL
+     *
+     * @return string 
+     */
+    public function getBaseURL()
+    {
+        return $this->baseURL;
+    }
 }
